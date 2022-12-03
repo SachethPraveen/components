7c9e0d976ae8e8670badea7118dd4559bdcb84ae
sachethpraveen <sacheth.praveen@card91.io>
2022-12-03 17:08:54 +0530

    modify index file to export components

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import * as React from 'react'
 import styles from './styles.module.css'
 import Card from './components/Card'
 import InputBox from './components/InputBox'
+import CardGroup from './components/CardGroup'
+import Header from './components/Header'
+import Table from './components/Table'
+import Sidebar from './components/Sidebar'
 
 interface Props {
   text: string
@@ -11,4 +15,4 @@ const ExampleComponent = ({ text }: Props) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
 
-export {Card,InputBox,ExampleComponent}
+export { Card, InputBox, ExampleComponent, CardGroup, Header, Table, Sidebar }
