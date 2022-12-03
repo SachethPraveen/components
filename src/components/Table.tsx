8179c6e24c601b4606302cca3b8d65e35202c362
sachethpraveen <sacheth.praveen@card91.io>
2022-12-03 17:13:50 +0530

    modify components

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,25 +1,24 @@
-import React from "react";
-import Filter from "./Filter";
+import React from 'react'
+import Filter from './Filter'
 
 const Table = (props: { data?: [] }) => {
   return (
-    <>
+    <div>
       {!props.data && (
-        <table className="table">
-          <thead className="table-light">
+        <table className='table'>
+          <thead className='table-light'>
             <tr>
-              <th scope="col">S. No</th>
-              <th scope="col">Transactions ID</th>
-              <th scope="col">Mobile No.</th>
-              <th scope="col">Bank</th>
-              <th scope="col">BIN</th>
-              <th scope="col">Status</th>
-              <th scope="col">Created Time</th>
+              <th scope='col'>S. No</th>
+              <th scope='col'>Transactions ID</th>
+              <th scope='col'>Mobile No.</th>
+              <th scope='col'>Bank</th>
+              <th scope='col'>BIN</th>
+              <th scope='col'>Status</th>
+              <th scope='col'>Created Time</th>
               <th></th>
             </tr>
           </thead>
           <tbody>
-            {/* <tr> */}
             <td>
               <Filter />
             </td>
@@ -41,74 +40,73 @@ const Table = (props: { data?: [] }) => {
             <td>
               <Filter />
             </td>
-            {/* </tr> */}
           </tbody>
           <tbody>
             <tr>
-              <th scope="row">1</th>
+              <th scope='row'>1</th>
               <td>123456789012345678901234567890</td>
               <td>1234567890</td>
               <td>YesBank</td>
               <td>123456</td>
-              <td className="text-success">SUCCESS</td>
+              <td className='text-success'>SUCCESS</td>
               <td>{Date()}</td>
               <td>
-                <span className="material-symbols-outlined">visibility</span>
+                <span className='material-symbols-outlined'>visibility</span>
               </td>
             </tr>
             <tr>
-              <th scope="row">2</th>
+              <th scope='row'>2</th>
               <td>123456789012345678901234567890</td>
               <td>1234567890</td>
               <td>YesBank</td>
               <td>123456</td>
-              <td className="text-danger">FAIL</td>
+              <td className='text-danger'>FAIL</td>
               <td>{Date()}</td>
               <td>
-                <span className="material-symbols-outlined">visibility</span>
+                <span className='material-symbols-outlined'>visibility</span>
               </td>
             </tr>
             <tr>
-              <th scope="row">3</th>
+              <th scope='row'>3</th>
               <td>123456789012345678901234567890</td>
               <td>1234567890</td>
               <td>YesBank</td>
               <td>123456</td>
-              <td className="text-success">SUCCESS</td>
+              <td className='text-success'>SUCCESS</td>
               <td>{Date()}</td>
               <td>
-                <span className="material-symbols-outlined">visibility</span>
+                <span className='material-symbols-outlined'>visibility</span>
               </td>
             </tr>
             <tr>
-              <th scope="row">4</th>
+              <th scope='row'>4</th>
               <td>123456789012345678901234567890</td>
               <td>1234567890</td>
               <td>YesBank</td>
               <td>123456</td>
-              <td className="text-danger">FAIL</td>
+              <td className='text-danger'>FAIL</td>
               <td>{Date()}</td>
               <td>
-                <span className="material-symbols-outlined">visibility</span>
+                <span className='material-symbols-outlined'>visibility</span>
               </td>
             </tr>
             <tr>
-              <th scope="row">5</th>
+              <th scope='row'>5</th>
               <td>123456789012345678901234567890</td>
               <td>1234567890</td>
               <td>YesBank</td>
               <td>123456</td>
-              <td className="text-success">SUCCESS</td>
+              <td className='text-success'>SUCCESS</td>
               <td>{Date()}</td>
               <td>
-                <span className="material-symbols-outlined">visibility</span>
+                <span className='material-symbols-outlined'>visibility</span>
               </td>
             </tr>
           </tbody>
         </table>
       )}
-    </>
-  );
-};
+    </div>
+  )
+}
 
-export default Table;
+export default Table
