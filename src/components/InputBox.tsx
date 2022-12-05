e87c57583e8b21dc931f754956264ef0e97f9271
sachethpraveen <sacheth.praveen@card91.io>
2022-12-05 15:39:14 +0530

    modify input box

--- src/components/InputBox.tsx
+++ src/components/InputBox.tsx
@@ -1,21 +1,59 @@
 import React from 'react'
 
-const InputBox = (props: { field?: string; fieldName?: string }) => {
+const InputBox = (props: {
+  keyPress: any
+  onChange: any
+  isUser: boolean
+  error: boolean
+  invalid: boolean
+  notFound: boolean
+}) => {
   return (
     <div className='mb-3 w-100'>
-      <label
-        htmlFor={props.field ? props.field : 'username'}
-        className='form-label w-100'
-      >
-        {props.fieldName ? props.fieldName : 'Enter user name'}
-      </label>
+      {!props.isUser && (
+        <>
+          <label htmlFor='username' className='form-label w-100'>
+            Enter user name
+          </label>
 
-      <input
-        type='text'
-        className='form-control w-100'
-        id={props.field ? props.field : 'username'}
-        placeholder='Username'
-      />
+          <input
+            type='text'
+            className='form-control w-100'
+            id='username'
+            placeholder='Username'
+            onKeyPress={props.keyPress}
+            onChange={props.onChange}
+          />
+
+          {props.error && (
+            <p className='text-danger'>Please enter a valid Username!</p>
+          )}
+
+          {props.notFound && <p className='text-danger'>User not Found!</p>}
+        </>
+      )}
+
+      {props.isUser && !props.error && (
+        <>
+          <label htmlFor='password' className='form-label w-100'>
+            Enter Password
+          </label>
+
+          <input
+            type='text'
+            className='form-control w-100'
+            id='password'
+            placeholder='Password'
+            onKeyPress={props.keyPress}
+            onChange={props.onChange}
+          />
+          {props.error && (
+            <p className='text-danger'>
+              Password should be atleast 8 characters!
+            </p>
+          )}
+        </>
+      )}
     </div>
   )
 }
