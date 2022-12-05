fbb08c8e9cd2ea2efde7518c3125c96f88fd7b5e
sachethpraveen <sacheth.praveen@card91.io>
2022-12-05 11:25:10 +0530

    modify header and sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,41 +1,44 @@
-import React from "react";
+import React from 'react'
+import { BsPerson } from 'react-icons/bs'
 
 const Header = (props: { name?: string }) => {
   return (
     <div
-      className="navbar d-flex justify-content-end bg-white w-100"
+      className='navbar d-flex justify-content-end bg-white w-100'
       style={{ zIndex: 2 }}
     >
-      <div className="dropdown me-3">
+      <div className='dropdown me-3'>
         <button
-          className="btn bg-white border-0 dropdown-toggle d-flex align-items-center"
-          type="button"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
+          className='btn bg-white border-0 dropdown-toggle d-flex align-items-center'
+          type='button'
+          data-bs-toggle='dropdown'
+          aria-expanded='false'
         >
-          <span className="material-symbols-outlined">person</span>
-          {props.name ? props.name : "User"}
+          <div className='fs-5 me-1'>
+            <BsPerson />
+          </div>
+          <div>{props.name ? props.name : 'User'}</div>
         </button>
-        <ul className="dropdown-menu">
+        <ul className='dropdown-menu'>
           <li>
-            <a className="dropdown-item" href="#">
+            <a className='dropdown-item' href='#'>
               Action
             </a>
           </li>
           <li>
-            <a className="dropdown-item" href="#">
+            <a className='dropdown-item' href='#'>
               Another action
             </a>
           </li>
           <li>
-            <a className="dropdown-item" href="#">
+            <a className='dropdown-item' href='#'>
               Something else here
             </a>
           </li>
         </ul>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,44 +1,56 @@
-import React, { useState } from "react";
-import "./Sidebar.css";
+import React, { useState } from 'react'
+import './Sidebar.css'
+import { BiHomeAlt } from 'react-icons/bi'
+import { CiMoneyBill } from 'react-icons/ci'
+import { GiSwipeCard } from 'react-icons/gi'
+import { IoCardOutline } from 'react-icons/io5'
 
 const Sidebar = () => {
-  const [mini, setMini] = useState(true);
+  const [mini, setMini] = useState(true)
   return (
     <div
-      className={mini ? "mini sidebar" : "sidebar"}
+      className={mini ? 'mini sidebar' : 'sidebar'}
       onMouseOver={() => {
-        console.log("over");
-        setMini((prevMini) => !prevMini);
+        console.log('over')
+        setMini((prevMini) => !prevMini)
       }}
       onMouseOut={() => {
-        console.log("out");
-        setMini((prevMini) => !prevMini);
+        console.log('out')
+        setMini((prevMini) => !prevMini)
       }}
     >
-      <a href="#">
-        <span className="material-symbols-outlined">home</span>
-        <span className="icon-text">Home</span>
+      <a href='#'>
+        <span className='material-symbols-outlined'>
+          <BiHomeAlt />
+        </span>
+        <span className='icon-text'>Home</span>
       </a>
       <br />
 
-      <a href="#">
-        <span className="material-symbols-outlined">money</span>{" "}
-        <span className="icon-text">Transactions</span>
+      <a href='#'>
+        <span className='material-symbols-outlined'>
+          <CiMoneyBill />
+        </span>{' '}
+        <span className='icon-text'>Transactions</span>
       </a>
       <br />
 
-      <a href="#">
-        <span className="material-symbols-outlined">payments</span>{" "}
-        <span className="icon-text">Cards</span>
+      <a href='#'>
+        <span className='material-symbols-outlined'>
+          <GiSwipeCard />
+        </span>{' '}
+        <span className='icon-text'>Cards</span>
       </a>
       <br />
 
-      <a href="#">
-        <span className="material-symbols-outlined">credit_card</span>{" "}
-        <span className="icon-text">Users</span>
+      <a href='#'>
+        <span className='material-symbols-outlined'>
+          <IoCardOutline />
+        </span>{' '}
+        <span className='icon-text'>Users</span>
       </a>
     </div>
-  );
-};
+  )
+}
 
-export default Sidebar;
+export default Sidebar
