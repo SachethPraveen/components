a85903f02ad908f5da7c034575bd95e53e7e26b1
sachethpraveen <sacheth.praveen@card91.io>
2022-12-08 18:36:34 +0530

    modify icons in card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,15 +4,15 @@ const Card = (props: { name?: string; number?: number; logo?: string }) => {
   return (
     <div className='card d-flex flex-row align-items-center w-100 mt-3'>
       <div className='p-2 m-1 w-100 text-start'>
-        <div>{props.name ? props.name : 'Detail Name'}</div>
-        <div>
-          <h1>{props.number ? props.number : 0}</h1>
-        </div>
+        <div className='fs-6'>{props.name ? props.name : 'Detail Name'}</div>
+        <div className='fs-1'>{props.number ? props.number : 0}</div>
       </div>
 
       <div className='p-2 m-1'>
         {props.logo ? (
-          <span className='material-symbols-outlined'>{props.logo}</span>
+          <span className='logo material-symbols-outlined fs-1'>
+            {props.logo}
+          </span>
         ) : (
           'Logo'
         )}
