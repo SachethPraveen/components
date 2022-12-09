f0cd8107a49538cc9eadf80415ecce259dde0670
sachethpraveen <sacheth.praveen@card91.io>
2022-12-09 13:10:08 +0530

    add unique keys to table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import Filter from './Filter'
 import { AiOutlineEye } from 'react-icons/ai'
 
-const Table = (props: { data?: Object[]; filterPress?: any }) => {
+const Table = (props: {
+  data?: Object[]
+  filterPress?: any
+  generateKey: () => string
+}) => {
   return (
     <div>
       {props.data && (
@@ -10,26 +14,32 @@ const Table = (props: { data?: Object[]; filterPress?: any }) => {
           <thead className='table-light'>
             <tr>
               {Object.keys(props.data[0]).map((head) => {
-                return <th scope='col'>{head}</th>
+                return (
+                  <th scope='col' key={props.generateKey()}>
+                    {head}
+                  </th>
+                )
               })}
               <th></th>
             </tr>
           </thead>
           <tbody>
-            {Object.keys(props.data[0]).map((key) => {
-              return (
-                <td>
-                  <Filter filterPress={props.filterPress} head={key} />
-                </td>
-              )
-            })}
+            <tr>
+              {Object.keys(props.data[0]).map((key) => {
+                return (
+                  <td key={props.generateKey()}>
+                    <Filter filterPress={props.filterPress} head={key} />
+                  </td>
+                )
+              })}
+            </tr>
           </tbody>
           <tbody>
             {props.data.map((row) => {
               return (
-                <tr>
+                <tr key={props.generateKey()}>
                   {Object.values(row).map((detail) => {
-                    return <td>{detail}</td>
+                    return <td key={props.generateKey()}>{detail}</td>
                   })}
                 </tr>
               )
