c6e7716245268ee3df851df758ebb748d0f54214
sachethpraveen <sacheth.praveen@card91.io>
2022-12-16 12:51:02 +0530

    modify input box component

--- src/components/InputBox.tsx
+++ src/components/InputBox.tsx
@@ -4,35 +4,33 @@ const InputBox = (props: {
   keyPress: any
   onChange: any
   isUser: boolean
-  error: boolean
+  userError: boolean
+  passError: boolean
   invalid: boolean
   notFound: boolean
-  onClick: any
 }) => {
   return (
     <div className='mb-3 w-100'>
-      {!props.isUser && (
-        <div>
-          <label htmlFor='username' className='form-label w-100'>
-            Enter user name
-          </label>
+      <div className='mb-3'>
+        <label htmlFor='username' className='form-label w-100'>
+          Enter user name
+        </label>
 
-          <input
-            type='text'
-            className='form-control w-100'
-            id='username'
-            placeholder='Username'
-            onKeyPress={props.keyPress}
-            onChange={props.onChange}
-          />
+        <input
+          type='text'
+          className='form-control w-100'
+          id='username'
+          placeholder='Username'
+          onKeyDown={props.keyPress}
+          onChange={props.onChange}
+        />
 
-          {props.error && (
-            <p className='text-danger'>Please enter a valid Username!</p>
-          )}
+        {props.userError && (
+          <p className='text-danger'>Please enter a valid Username!</p>
+        )}
 
-          {props.notFound && <p className='text-danger'>User not Found!</p>}
-        </div>
-      )}
+        {props.notFound && <p className='text-danger'>User not Found!</p>}
+      </div>
 
       {props.isUser && (
         <div>
@@ -45,15 +43,11 @@ const InputBox = (props: {
             className='form-control w-100'
             id='password'
             placeholder='Password'
-            onKeyPress={props.keyPress}
+            onKeyDown={props.keyPress}
             onChange={props.onChange}
           />
 
-          <div className='text-secondary mb-3' onClick={props.onClick}>
-            Change username
-          </div>
-
-          {props.error && (
+          {props.passError && (
             <p className='text-danger'>
               Password should be atleast 8 characters!
             </p>
