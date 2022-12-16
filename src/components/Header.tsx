da4b5ea8ef5a7a02e54105f2ccc9f179ad14df11
sachethpraveen <sacheth.praveen@card91.io>
2022-12-16 14:51:13 +0530

    modify header logout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BsPerson } from 'react-icons/bs'
 
-const Header = (props: { name?: string }) => {
+const Header = (props: { name?: string; onClick: any }) => {
   return (
     <div
       className='navbar d-flex justify-content-end bg-white w-100'
@@ -21,7 +21,7 @@ const Header = (props: { name?: string }) => {
         </button>
         <ul className='headerDrop dropdown-menu'>
           <li>
-            <a className='dropdown-item' href='/'>
+            <a className='dropdown-item' onClick={props.onClick}>
               Logout
               <span className='material-symbols-outlined align-middle ms-2'>
                 power_rounded
